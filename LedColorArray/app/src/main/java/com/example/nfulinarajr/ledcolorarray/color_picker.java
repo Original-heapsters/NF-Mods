4393ca84f4e35722c2ba238671e9d933ebeae001
Napoleon Fulinara Jr <nfulinarajr@users.noreply.github.com>
2017-01-21 15:25:02 -0800

    initial project

---
+++ LedColorArray/app/src/main/java/com/example/nfulinarajr/ledcolorarray/color_picker.java
@@ -0,0 +1,13 @@
+package com.example.nfulinarajr.ledcolorarray;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class color_picker extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_color_picker);
+    }
+}
